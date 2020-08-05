e4cf15ff37e72510f0aa2adff6a321676b1eec0a
praveshcj <praveshjain500@gmail.com>
2020-08-05 16:28:28 +0530

    First Portfolio Update

--- app.py
+++ app.py
@@ -1,29 +1,21 @@
 # app.py
-from flask import Flask, request, jsonify
-app = Flask(__name__)
+from flask import Flask, request, jsonify, send_file, render_template
+app = Flask(__name__, static_folder='./build', static_url_path='/')
+# app= Flask(__name__)
 
-@app.route('/getmsg/', methods=['GET'])
+
+@app.route('/')
+def index():
+    template = open('./build/index.html', 'r');
+    print(template);
+    return app.send_static_file('index.html')
+
+
+
+@app.route('/profilepic', methods=['GET'])
 def respond():
-    # Retrieve the name from url parameter
-    name = request.args.get("name", None)
-
-    # For debugging
-    print(f"got name {name}")
-
-    response = {}
-
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
-
-    # Return the response in json format
-    return jsonify(response)
+    pic = open('profilepic.jpg', 'rb')
+    return send_file(pic, mimetype='image/JPG')
 
 @app.route('/post/', methods=['POST'])
 def post_something():
@@ -42,9 +34,7 @@ def post_something():
         })
 
 # A welcome message to test our server
-@app.route('/')
-def index():
-    return "<h1>Welcome to our server !!</h1>"
+
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
---
+++ build/precache-manifest.b040864175285848c62830f1246daf5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "560835a83e9c3287c5f9",
+    "url": "/static/css/main.19cfd055.chunk.css"
+  },
+  {
+    "revision": "560835a83e9c3287c5f9",
+    "url": "/static/js/main.560835a8.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "076eb64f1448c308eb64083b356ad196",
+    "url": "/index.html"
+  }
+];
